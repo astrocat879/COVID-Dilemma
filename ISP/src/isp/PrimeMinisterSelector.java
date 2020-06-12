e03065fbe49b5a4fafa12159f6c0e42e65fea2e6
hewmatt10 <hewmatt10@gmail.com>
2020-06-12 13:21:34 -0400

    added 2 classes

---
+++ ISP/src/isp/PrimeMinisterSelector.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package isp;
+
+/**
+ *
+ * @author hewmatt
+ */
+import java.util.*;
+public class PrimeMinisterSelector {
+    ArrayList<Event> events;
+    ArrayList<Boolean> use;
+    public PrimeMinisterSelector(){
+        events = new ArrayList<>();
+        use = new ArrayList<>();
+    }
+    public void add(Event ee){
+        events.add(ee);
+        use.add(false);
+    }
+    public int select(){
+        while(true){
+            int id = (int)(Math.random()*(events.size()));
+            if(use.get(id) == false){
+                return id;
+            }
+        }
+    }
+    public Event getEvent(int id){
+        return events.get(id);
+    }
+    public void complete(int id){
+        use.set(id,true);
+    }
+}
