82518641374df978c9459a7b080d9750e615cddb
RonaldYou <ronaldrenyou@gmail.com>
2020-06-15 18:31:09 -0400

    moreendstuff

---
+++ ISP/src/isp/FEnd.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package isp;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *Revision History:
+ *  - June 11th, 2020: Created by Ronald You
+ *  - 
+ * 
+ */
+public class FEnd {
+     public static void run(Stage window){
+        final String b_IDLE_BUTTON_STYLE = " -fx-background-color: #8e7cc3; ; -fx-text-fill: #ffffff";
+        final String b_HOVERED_BUTTON_STYLE = "-fx-background-color: #674ea7;  -fx-text-fill: #ffffff;";
+        final String b_CLICKED_BUTTON_STYLE = "-fx-background-color: #674ea7; ";
+        
+        
+        Pane root4 = new Pane();
+        ISP.s7 = new Scene(root4, 1200, 750);
+        Image img  = new Image("FEnd.jpg");
+        root4.getChildren().add(new ImageView(img));
+        
+        
+        
+        Button b = new Button("Continue");
+        b.relocate(1105,715);
+        b.setStyle(b_IDLE_BUTTON_STYLE);
+        b.setOnMouseEntered(e -> b.setStyle(b_HOVERED_BUTTON_STYLE));
+        b.setOnMouseExited(e -> b.setStyle(b_IDLE_BUTTON_STYLE));
+        b.setOnMousePressed(e -> b.setStyle(b_CLICKED_BUTTON_STYLE));
+        b.setOnAction(e -> {
+            try{Thread.sleep(100);}
+            catch(Exception f){}
+            window.setScene(ISP.s0);
+        });
+        root4.getChildren().add(b);
+    }
+   
+    
+}
